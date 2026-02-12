125c81932f6f27dbd043ce9eeb6978e58fde7ad6
MarcosDev <marcosmoruadev@gmail.com>
2026-02-12 16:34:42 -0300

    config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   experimental: {
     turbopackFileSystemCacheForDev: true,
   },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
